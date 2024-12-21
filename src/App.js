1fa7d195c37fa7455204afabbfea53967cfec584
sathvika <sathvikavalmiki@gmail.com>
2024-12-21 10:44:24 +0530

    Adding all files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Apple from './Apple';
+import Orange from './Orange';
+function App() {
+  return (
+    <div className="App">
+      <Apple name="Sathvika"/>
+      <Orange />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Orange.jsx
@@ -0,0 +1,7 @@
+function Orange()
+{
+    return( 
+        <h1>Hi This is an Orange </h1>
+    )
+}
+export default Orange;
